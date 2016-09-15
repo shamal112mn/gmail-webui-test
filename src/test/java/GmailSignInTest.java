92b25e8a40200093c9c461854c8cb47918cb82e7
shamal112mn <azatmurataliev@gmail.com>
2016-09-15 16:55:47 -0500

    refactor with pageObject partern and used for signOut method MouseActions 1

--- src/test/java/GmailSignInTest.java
+++ src/test/java/GmailSignInTest.java
@@ -17,66 +17,66 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GmailSignInTest{
     WebDriver dr = new FirefoxDriver();
-    WebDriverWait wd = new WebDriverWait(dr, 30);
+
 
 
     @Test
     public void gmailLoginShouldBeSuccessful(){
 
 // SignIn
-        // vcs 8
-        SignInPage signInPage = WebUtil.goToSignInPage(dr, wd);
+        // code refactoring complete for signIn part
+        SignInPage signInPage = WebUtil.goToSignInPage(dr);
 
-        signInPage.fillInUsername(dr,wd, "nazymhealthywater");
+        signInPage.fillInUsername(dr);
 
-        signInPage.fillInPassword(dr, wd, "Arlen@470317");
+        signInPage.fillInPassword(dr);
 
-       EmailHomePage emailHomePage = signInPage.clickSignIn(dr, wd);
+       EmailHomePage emailHomePage = signInPage.clickSignIn(dr);
 
-        Assert.assertTrue("Inbox should exist", emailHomePage.isInboxExist(dr, wd));
+        Assert.assertTrue("Inbox should exist", emailHomePage.isInboxExist(dr));
 
 
         // SIGNOUT
 
-        signInPage  = emailHomePage.signOut(dr, wd);
+        signInPage  = emailHomePage.signOut(dr);
 
-        Assert.assertTrue("signIn button should exist",signInPage.isSignInButtonExist(dr, wd));
+        Assert.assertTrue("signIn button should exist",signInPage.isSignInButtonExist(dr));
     }
     @Test
     public void gmailSendAndReseiveEmailTest(){
-        SignInPage signInPage = WebUtil.goToSignInPage(dr, wd);
+        SignInPage signInPage = WebUtil.goToSignInPage(dr);
 
-        signInPage.fillInUsername(dr,wd, "nazymhealthywater");
+        signInPage.fillInUsername(dr);
 
-        signInPage.fillInPassword(dr, wd, "Arlen@470317");
+        signInPage.fillInPassword(dr);
 
-        EmailHomePage emailHomePage = signInPage.clickSignIn(dr, wd);
+        EmailHomePage emailHomePage = signInPage.clickSignIn(dr);
 
-        Assert.assertTrue("Inbox should exist", emailHomePage.isInboxExist(dr, wd));
+        Assert.assertTrue("Inbox should exist", emailHomePage.isInboxExist(dr));
 
             //COMPOSE EMAIL
-        emailHomePage.clickComposeButton(dr, wd);
+        emailHomePage.clickComposeButton(dr);
 
         String recipient = "nazymhealthywater@gmail.com";
-        emailHomePage.fillInRecipient(dr, wd, recipient);
+        emailHomePage.fillInRecipient(dr, recipient);
 
         String subject = "New message for you";
-        emailHomePage.fillInSubjectLine(dr, wd, subject);
+        emailHomePage.fillInSubjectLine(dr, subject);
 
 
         String bodyMessage = "Today is good time to code app";
-        emailHomePage.fillInEmailBody(dr,wd,bodyMessage);
+        emailHomePage.fillInEmailBody(dr,bodyMessage);
 
 
         emailHomePage.clickSendEmail(dr);
 
-        emailHomePage.clickInboxWithNewEmail(dr,wd);
+        emailHomePage.clickInboxWithNewEmail(dr);
 
-        EmailViewPage emailViewPage = emailHomePage.clickNewEmail(dr,wd);
+        EmailViewPage emailViewPage = emailHomePage.clickNewEmail(dr);
 
-        emailViewPage.getEmailSubjectText(dr, wd);
+        emailViewPage.getEmailSubjectText(dr);
 
-        Assert.assertEquals("Email subject text should be the same", subject, emailViewPage.getEmailSubjectText(dr,wd));
+        Assert.assertEquals("Email subject text should be the same", subject, emailViewPage.getEmailSubjectText(dr));
 
         emailViewPage.getEmailBodyText(dr);
 
@@ -86,14 +86,15 @@ public class GmailSignInTest{
         // SIGNOUT
 
         //Actions ms = new Actions(dr);
-        emailHomePage.signOut(dr,wd);
 
+        signInPage = emailHomePage.signOut(dr);
+        Assert.assertTrue("signIn button should exist",signInPage.isSignInButtonExist(dr));
     }
 
     @After
     public  void closeBrowser(){
         // changes
 
-        dr.close();
+       dr.close();
     }
 }
