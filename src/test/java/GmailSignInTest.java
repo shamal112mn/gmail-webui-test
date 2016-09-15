071fadad7713689113aad51ba6731961cc171c00
shamal112mn <azatmurataliev@gmail.com>
2016-09-14 20:30:06 -0500

    send email and verify 8

--- src/test/java/GmailSignInTest.java
+++ src/test/java/GmailSignInTest.java
@@ -23,7 +23,7 @@ public class GmailSignInTest{
     public void gmailLoginShouldBeSuccessful(){
 
 // SignIn
-        // vcs 7
+        // vcs 8
         dr.get("http://gmail.com/");
         System.out.println(dr.getTitle());
         dr.findElement(By.xpath("//input[@id='Email']")).sendKeys("nazymhealthywater");
